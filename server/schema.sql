f09b1d8fae7634e0cd60a8926077959747262545
millionhari <mikemhari@gmail.com>
2015-02-27 16:09:54 -0800

    Implement chat and built some of the schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,36 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  username varchar(50),
+  message varchar(50)
 );
 
+-- CREATE TABLE users (
+--   username varchar(50),
+--   roomname varchar(50),
+--   message varchar(140)
+-- );
+
+-- CREATE TABLE rooms (
+--   username varchar(50),
+--   roomname varchar(50),
+--   message varchar(140)
+-- );
+
 /* Create other tables and define schemas for them here! */
-
-
+/*
+1) Users
+    - username
+    - roomname
+    -message
+2) Messages
+    - username
+    - message
+3) Rooms
+    - username
+    - roomname
+    - message
+*/
 
 
 /*  Execute this file from the command line by typing:
