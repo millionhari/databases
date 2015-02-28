4d2e174bc2261474f70a8b3bfa6846c2e46e978c
millionhari <mikemhari@gmail.com>
2015-02-27 16:51:28 -0800

    Able to read from SQL database

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,21 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'chat',
+  password: ''
+});
 
+connection.connect();
+
+connection.query('SELECT * from messages', function(err, rows, fields){
+  if(err){
+    console.log('ERROR!');
+  }
+
+  console.log('The message is', rows);
+});
+
+connection.end();
